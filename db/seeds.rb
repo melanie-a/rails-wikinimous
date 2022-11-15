a76cd881f6fd480166dd4fb39b07b668763c48d0
Melanie Armitano <melanie.armi@gmail.com>
2022-11-15 17:05:13 +0100

    rails wikinomous

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Name.name,
+    content: Faker::Lorem.sentences
+  )
+  article.save!
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
